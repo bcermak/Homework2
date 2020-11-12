a06085031927dc5e72226c7e986fde3c428c17cb
Brandon Cermak <cermakprofessional@gmail.com>
2020-11-12 15:00:50 -0600

    new layout for projects

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var path = require("path");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -13,7 +14,7 @@ var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('/css/style.css'))  
+app.use(express.static(path.join(__dirname, 'css/style.css')));
 
 // ================================================================================
 // ROUTER
